329802e79b937b1620b72ca5df9b67844de5a317
chenyitao <chenyitao@zhuanzhuan.com>
2020-12-22 00:14:16 +0800

    feat: 创建"回文字符串"文件

---
+++ src/leetcode/validatePalidrome/validatePalidrome.test.ts
@@ -0,0 +1,5 @@
+import sumIndexOfTarget from './validatePalidrome';
+
+test('sumIndexOfTarget', () => {
+  expect('dasabcbafsdj').toBeTruthy();
+});
---
+++ src/leetcode/validatePalidrome/validatePalidrome.ts
@@ -0,0 +1,9 @@
+type ValidatePalidrome = (s: string) => boolean
+
+const defaultSolution: ValidatePalidrome = (s: string) => {
+  return true;
+};
+
+export default {
+  defaultSolution
+};
