c366a75cd6e39101777d1093420219af7a810050
chenyitao <chenyitao@zhuanzhuan.com>
2021-06-06 23:56:56 +0800

    feat: implement stack using queues

--- src/leetcode/myStack/myStack.ts
+++ src/leetcode/myStack/myStack.ts
@@ -1,14 +1,28 @@
 // https://leetcode-cn.com/problems/implement-stack-using-queues/
 
 export class MyStack {
-  queue1 = [];
-  queue2 = [];
+  queue = [];
+  temp = [];
 
-  push(x: number): void {}
+  push(x: number): void {
+    while (this.queue.length !== 0) {
+      this.temp.push(this.queue.shift())
+    }
+    this.queue.push(x)
+    while (this.temp.length !== 0) {
+      this.queue.unshift(this.temp.pop())
+    }
+  }
 
-  pop(): number {}
+  pop(): number {
+    return this.queue.pop()
+  }
 
-  top(): number {}
+  top(): number {
+    return this.queue[this.queue.length - 1]
+  }
 
-  empty(): boolean {}
+  empty(): boolean {
+    return this.queue.length === 0
+  }
 }
