daab1a04b45f702566f3d646a920dc34b8b9de5e
chenyitao <chenyitao@zhuanzhuan.com>
2021-06-11 14:07:41 +0800

    feat: max sliding window, the O(n) solution

--- src/leetcode/maxSlidingWindow/maxSlidingWindow.ts
+++ src/leetcode/maxSlidingWindow/maxSlidingWindow.ts
@@ -1,33 +1,40 @@
 // https://leetcode-cn.com/problems/sliding-window-maximum/
 
-// export function maxSlidingWindow(nums: number[], k: number): number[] {
-//   let left = 0,
-//     right = k - 1;
-//   const maxSlidingWindow: Array<number> = [];
-//   let max = nums[left];
-//   let index = left;
-//   while (index <= right) {
-//     if (nums[index] > max) {
-//       max = nums[index];
-//     }
-//     index++;
-//   }
-//   while (right < nums.length) {
-//     if (nums[right] > max) {
-//       max = nums[right];
-//     }
-//     maxSlidingWindow.push(max);
-//     left++;
-//     right++;
-//   }
-//   return maxSlidingWindow;
-// }
-
 export function maxSlidingWindow(nums: number[], k: number): number[] {
-  let queue = []
+  // keep numbers in queue ranked from bigger to smaller.
+  // before push new number into queue, pop all numbers that's
+  // smaller than the new number.
+  const queue = [];
+  const result = [];
+  // 1. push first k number into queue
   for (let i = 0; i < k; i++) {
-    nums[i]
+    while (queue.length > 0 && nums[queue[queue.length - 1]] < nums[i]) {
+      queue.pop();
+    }
+    queue.push(i);
   }
+  result.push(nums[queue[0]])
+
+  for (let i = k; i < nums.length; i++) {
+    // pop all numbers that's smaller than the new number.
+    while (queue.length > 0 && nums[queue[queue.length - 1]] < nums[i]) {
+      queue.pop();
+    }
+    // push index rather than the true value, for convenience of comparing the
+    // index of numbers, to see whether it's within sliding window.
+    queue.push(i);
+    // the first number in queue is the biggest and might be the number
+    // we're looking for. we should ensure its index is within the sliding
+    // window.
+    while (queue.length > 0 && queue[0] < (i - k + 1)) {
+      queue.shift();
+    }
+    // then the number at index 0 is the biggest in the sliding window.
+    result.push(nums[queue[0]])
+  }
+
+
+  return result;
 }
 
 
