bc781c219e6218a9cb3aa2b82ae024e016b0fdab
chenyitao <chenyitao@zhuanzhuan.com>
2021-06-14 17:36:01 +0800

    chore: remove levelorderTraversal test file

--- src/leetcode/levelorderTraversal/levelorderTraversal.test.ts
+++
@@ -1,8 +0,0 @@
-import {inorderTraversal, TreeNode} from './inorderTraversal';
-
-test('postorderTraversal0', () => {
-  const result = inorderTraversal(
-    new TreeNode(1, null, new TreeNode(2, new TreeNode(3), null))
-  );
-  expect(result).toStrictEqual([1, 3, 2]);
-});
