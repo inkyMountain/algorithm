9a061b43aab8c66b18e076c4228b50efbb6e889f
chenyitao <chenyitao@zhuanzhuan.com>
2021-06-23 23:53:37 +0800

    feat: combinationSum

--- src/leetcode/combinationSum/combinationSum.ts
+++ src/leetcode/combinationSum/combinationSum.ts
@@ -4,5 +4,27 @@ export function combinationSum(
   candidates: number[],
   target: number
 ): number[][] {
+  let nums = [];
+  let result = [];
+  let sum = 0;
 
+  function helper(startIndex: number) {
+    if (sum === target) {
+      result.push([...nums]);
+      return;
+    }
+    if (sum > target) {
+      return;
+    }
+    for (let i = startIndex; i < candidates.length; i++) {
+      nums.push(candidates[i]);
+      sum += candidates[i];
+      helper(i);
+      sum -= candidates[i];
+      nums.pop();
+    }
+  }
+
+  helper(0);
+  return result;
 }
