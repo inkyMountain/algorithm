7d94c981c70e448f9817181daacb0073dee1376b
chenyitao <chenyitao@zhuanzhuan.com>
2021-06-23 13:30:48 +0800

    feat: letterCombinations

--- src/leetcode/letterCombinations/letterCombinations.ts
+++ src/leetcode/letterCombinations/letterCombinations.ts
@@ -1,19 +1,39 @@
 // https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
 
-export class TreeNode {
-  val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
-
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.left = left === undefined ? null : left;
-    this.right = right === undefined ? null : right;
-  }
-}
+const letterMap = {
+  2: ['a', 'b', 'c'],
+  3: ['d', 'e', 'f'],
+  4: ['g', 'h', 'i'],
+  5: ['j', 'k', 'l'],
+  6: ['m', 'n', 'o'],
+  7: ['p', 'q', 'r', 's'],
+  8: ['t', 'u', 'v'],
+  9: ['w', 'x', 'y', 'z'],
+};
 
 export function letterCombinations(digits: string): string[] {
+  if (digits === '') {
+    return []
+  }
+  const result = [];
+  let combo = [];
+  let index = 0;
 
+  function recurse() {
+    if (combo.length === digits.length) {
+      result.push(combo.join(''));
+      return;
+    }
+    const matchedLetters = letterMap[digits[index]];
+    for (let i = 0; i < matchedLetters.length; i++) {
+      combo.push(matchedLetters[i]);
+      index++;
+      recurse();
+      combo.pop();
+      index--;
+    }
+  }
+
+  recurse();
+  return result;
 }
-
-const helper = () => {};
