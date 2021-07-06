70cbb9269a19d689197ca19b1305440ebbbe4feb
chenyitao <chenyitao@zhuanzhuan.com>
2021-07-06 23:45:53 +0800

    feat: setup solveSudoku.ts

---
+++ src/leetcode/solveSudoku/solveSudoku.test.ts
@@ -0,0 +1,9 @@
+import {solveSudoku} from './solveSudoku'
+
+test('solveSudoku0', () => {
+  // const result = solveSudoku(4)
+  // expect(result).toStrictEqual([
+  //   ['.Q..', '...Q', 'Q...', '..Q.'],
+  //   ['..Q.', 'Q...', '...Q', '.Q..'],
+  // ])
+})
---
+++ src/leetcode/solveSudoku/solveSudoku.ts
@@ -0,0 +1,5 @@
+// https://leetcode-cn.com/problems/sudoku-solver/
+
+export function solveSudoku(board: string[][]): void {
+
+};
