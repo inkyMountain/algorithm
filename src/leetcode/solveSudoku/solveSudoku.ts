6a0065c2395c1da1ad250ebd41569b9535754435
chenyitao <chenyitao@zhuanzhuan.com>
2021-07-11 16:13:08 +0800

    feat: solveSudoku.ts

--- src/leetcode/solveSudoku/solveSudoku.ts
+++ src/leetcode/solveSudoku/solveSudoku.ts
@@ -1,9 +1,31 @@
 // https://leetcode-cn.com/problems/sudoku-solver/
 
+export const nextCoordinate = (
+  x: number,
+  y: number,
+  board: string[][]
+): {x: number; y: number} => {
+  if (x === 8) {
+    x = 0
+    y = y + 1
+  } else {
+    x = x + 1
+  }
+
+  if (board[y] === undefined || board[y][x] === '.') {
+    return {x, y}
+  } else {
+    return nextCoordinate(x, y, board)
+  }
+}
+
 export function solveSudoku(board: string[][]): void {
   const isValid = (num: number, x: number, y: number) => {
     const used0 = new Array(9).fill(false)
     for (let i = 0; i < 9; i++) {
+      if (board[i][x] === '.') {
+        continue
+      }
       const current = parseInt(board[i][x])
       if (used0[current - 1]) {
         return false
@@ -14,6 +36,9 @@ export function solveSudoku(board: string[][]): void {
 
     const used1 = new Array(9).fill(false)
     for (let i = 0; i < 9; i++) {
+      if (board[y][i] === '.') {
+        continue
+      }
       const current = parseInt(board[y][i])
       if (used1[current - 1]) {
         return false
@@ -23,40 +48,43 @@ export function solveSudoku(board: string[][]): void {
     }
 
     const used2 = new Array(9).fill(false)
-    for (let i = 0; i < 9; i++) {
-      const current = parseInt(board[y][i])
-      if (used1[current - 1]) {
-        return false
-      } else {
-        used1[current - 1] = true
+    const startX = parseInt((x / 3).toString()) * 3
+    const startY = parseInt((y / 3).toString()) * 3
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[startY + j][startX + i] === '.') {
+          continue
+        }
+        const current = parseInt(board[startY + j][startX + i])
+        if (used2[current - 1]) {
+          return false
+        } else {
+          used2[current - 1] = true
+        }
       }
     }
-  }
-  const nextCoordinate = (x: number, y: number): {x: number; y: number} => {
-    if (x === 9) {
-      x = 0
-      y = y + 1
-    } else {
-      x = x + 1
-    }
-    return {x, y}
+    return true
   }
   const recurse = (x: number, y: number) => {
-    const needFill = board[y][x] === '.'
-    if (!needFill) {
-      return
+    if (y === 9) {
+      return true
     }
-
     for (let i = 1; i <= 9; i++) {
       const original = board[y][x]
       board[y][x] = i.toString()
-      if (isValid(i, x, y)) {
-        const next = nextCoordinate(x, y)
-        recurse(next.x, next.y)
+      const isCurrentNumberValid = isValid(i, x, y)
+      if (isCurrentNumberValid) {
+        const next = nextCoordinate(x, y, board)
+        if (recurse(next.x, next.y)) {
+          return true
+        } else {
+          board[y][x] = original
+        }
       } else {
         board[y][x] = original
       }
     }
   }
-  recurse(0, 0)
+  const next = nextCoordinate(-1, 0, board)
+  recurse(next.x, next.y)
 }
