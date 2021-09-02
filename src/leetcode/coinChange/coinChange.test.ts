5e4f65754359a52091c837804da7af540c842884
chenyitao <chenyitao@zhuanzhuan.com>
2021-09-02 13:14:33 +0800

    feat: coinChange.ts

--- src/leetcode/coinChange/coinChange.test.ts
+++ src/leetcode/coinChange/coinChange.test.ts
@@ -1,21 +1,21 @@
-import {change} from './coinChange'
+import {coinChange} from './coinChange'
 
 test('change0', () => {
-  expect(change([1, 2, 5], 11)).toStrictEqual(3)
+  expect(coinChange([1, 2, 5], 11)).toStrictEqual(3)
 })
 
 test('change1', () => {
-  expect(change([2], 3)).toStrictEqual(-1)
+  expect(coinChange([2], 3)).toStrictEqual(-1)
 })
 
 test('change2', () => {
-  expect(change([1], 0)).toStrictEqual(0)
+  expect(coinChange([1], 0)).toStrictEqual(0)
 })
 
 test('change3', () => {
-  expect(change([1], 1)).toStrictEqual(1)
+  expect(coinChange([1], 1)).toStrictEqual(1)
 })
 
 test('change4', () => {
-  expect(change([1], 2)).toStrictEqual(2)
+  expect(coinChange([1], 2)).toStrictEqual(2)
 })
--- src/leetcode/coinChange/coinChange.ts
+++ src/leetcode/coinChange/coinChange.ts
@@ -1,4 +1,15 @@
 // https://leetcode-cn.com/problems/coin-change/
 
-export function change(coins: number[], amount: number, ): number {
+export function coinChange(coins: number[], amount: number): number {
+  const dp = new Array(amount + 1).fill(Infinity)
+  dp[0] = 0
+  for (let i = 0; i < coins.length; i++) {
+    for (let j = 0; j <= amount; j++) {
+      if (j >= coins[i]) {
+        dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1)
+      }
+    }
+  }
+  const result = dp[amount]
+  return result === Infinity ? -1 : result
 }
