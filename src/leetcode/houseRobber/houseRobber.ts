b5ecf049d3bf86c6be2d304fbc08665673e33d7e
chenyitao <chenyitao@zhuanzhuan.com>
2021-09-14 10:18:36 +0800

    chore: tweak house robber

--- src/leetcode/houseRobber/houseRobber.ts
+++ src/leetcode/houseRobber/houseRobber.ts
@@ -4,9 +4,7 @@ export function rob(nums: number[]): number {
   const dp = new Array(nums.length).fill(0)
   dp[0] = nums[0]
   for (let i = 1; i < nums.length; i++) {
-    const xxx = dp[i - 1]
-    const yyy = (dp[i - 2] || 0) + nums[i]
-    dp[i] = Math.max(xxx, yyy)
+    dp[i] = Math.max(dp[i - 1], (dp[i - 2] || 0) + nums[i])
   }
   return dp[dp.length - 1]
 }
