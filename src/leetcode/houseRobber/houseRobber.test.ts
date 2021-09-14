c2d61de2336d8ac97759329db0c140128127113e
chenyitao <chenyitao@zhuanzhuan.com>
2021-09-14 10:17:56 +0800

    feat: house robber

--- src/leetcode/houseRobber/houseRobber.test.ts
+++ src/leetcode/houseRobber/houseRobber.test.ts
@@ -7,3 +7,11 @@ test('rob0', () => {
 test('rob1', () => {
   expect(rob([2, 7, 9, 3, 1])).toStrictEqual(12)
 })
+
+test('rob2', () => {
+  expect(rob([1, 2])).toStrictEqual(2)
+})
+
+test('rob3', () => {
+  expect(rob([1, 3, 1])).toStrictEqual(3)
+})
--- src/leetcode/houseRobber/houseRobber.ts
+++ src/leetcode/houseRobber/houseRobber.ts
@@ -1,6 +1,12 @@
 // https://leetcode-cn.com/problems/house-robber/
 
 export function rob(nums: number[]): number {
-  const dp = []
-  
-};
+  const dp = new Array(nums.length).fill(0)
+  dp[0] = nums[0]
+  for (let i = 1; i < nums.length; i++) {
+    const xxx = dp[i - 1]
+    const yyy = (dp[i - 2] || 0) + nums[i]
+    dp[i] = Math.max(xxx, yyy)
+  }
+  return dp[dp.length - 1]
+}
