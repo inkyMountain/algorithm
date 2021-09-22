cec84817ad492eb1ed744f547c4fb5bc803c326b
chenyitao <chenyitao@zhuanzhuan.com>
2021-09-22 14:10:27 +0800

    feat: another bestTimeToBuyAndSellStockWithTransactionFee

--- src/leetcode/bestTimeToBuyAndSellStockWithTransactionFee/bestTimeToBuyAndSellStockWithTransactionFee.ts
+++ src/leetcode/bestTimeToBuyAndSellStockWithTransactionFee/bestTimeToBuyAndSellStockWithTransactionFee.ts
@@ -14,13 +14,24 @@
 //   return dp[prices.length - 1][0]
 // }
 
-// with rolling array
+// with rolling array, minus fee when **sell**
+// export function maxProfit(prices: number[], fee: number): number {
+//   let profit0 = 0
+//   let profit1 = -prices[0]
+//   for (let i = 1; i < prices.length; i++) {
+//     profit0 = Math.max(profit0, profit1 + prices[i] - fee)
+//     profit1 = Math.max(profit0 - prices[i], profit1)
+//   }
+//   return profit0
+// }
+
+// with rolling array, minus fee when **buy**
 export function maxProfit(prices: number[], fee: number): number {
   let profit0 = 0
-  let profit1 = -prices[0]
+  let profit1 = -prices[0] - fee
   for (let i = 1; i < prices.length; i++) {
-    profit0 = Math.max(profit0, profit1 + prices[i] - fee)
-    profit1 = Math.max(profit0 - prices[i], profit1)
+    profit0 = Math.max(profit0, profit1 + prices[i])
+    profit1 = Math.max(profit0 - prices[i] - fee, profit1)
   }
   return profit0
 }
