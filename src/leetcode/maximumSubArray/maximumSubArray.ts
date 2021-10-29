032ca5f1f8825d9f86085a81b0384901e4be5dfd
chenyitao <chenyitao@zhuanzhuan.com>
2021-10-29 21:56:37 +0800

    comment: maximumSubArray.ts

--- src/leetcode/maximumSubArray/maximumSubArray.ts
+++ src/leetcode/maximumSubArray/maximumSubArray.ts
@@ -1,3 +1,14 @@
+/**
+ * keypoint:
+ * 1. dp[i] means a contiguous numbers which ends at i.
+ * 2. the transition formula comes from
+ *  - current number
+ *  - or the previous max sub array plus current number
+ *
+ *  optimizations:
+ *  dp array can be replaced with a number, cause transition formula relies
+ *  only on previous status.
+ */
 export function maxSubArray(nums: number[]): number {
   const dp = new Array(nums.length).fill(0)
   dp[0] = nums[0]
