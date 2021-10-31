71ea00c46e546fe87554a7821c17998439a70796
chenyitao <chenyitao@zhuanzhuan.com>
2021-10-31 23:22:56 +0800

    feat: scaffold numberOfIslands

---
+++ src/leetcode/islandNumbers/numberOfIslands.ts
@@ -0,0 +1,14 @@
+// https://leetcode-cn.com/problems/number-of-islands/
+
+const LAND = '1'
+const WATER = '0'
+
+export function numIslands(grid: string[][]): number {
+  let total = 0
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      
+    }
+  }
+  return total
+}
