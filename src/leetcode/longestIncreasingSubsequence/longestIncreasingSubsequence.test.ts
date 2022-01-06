f3cd30290776435ff83c8f6687a6142a37b360e1
chenyitao <chenyitao@zhuanzhuan.com>
2022-01-06 22:37:29 +0800

    feat: longestIncreasingSubsequence

--- src/leetcode/longestIncreasingSubsequence/longestIncreasingSubsequence.test.ts
+++ src/leetcode/longestIncreasingSubsequence/longestIncreasingSubsequence.test.ts
@@ -13,3 +13,19 @@ describe('longestIncreasingSubsequence', () => {
     expect(lengthOfLIS([2, 2, 2, 2, 2])).toStrictEqual(1)
   })
 })
+
+// describe('longestIncreasingSubsequence', () => {
+//   it('longestIncreasingSubsequence0', () => {
+//     expect(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])).toStrictEqual([
+//       2, 3, 5, 6,
+//     ])
+//   })
+
+//   it('longestIncreasingSubsequence1', () => {
+//     expect(lengthOfLIS([0, 1, 0, 3, 2, 3])).toStrictEqual([0, 1, 4, 5])
+//   })
+
+//   it('longestIncreasingSubsequence2', () => {
+//     expect(lengthOfLIS([2, 2, 2, 2, 2])).toStrictEqual([0])
+//   })
+// })
--- src/leetcode/longestIncreasingSubsequence/longestIncreasingSubsequence.ts
+++ src/leetcode/longestIncreasingSubsequence/longestIncreasingSubsequence.ts
@@ -1,13 +1,37 @@
 // https://leetcode-cn.com/problems/longest-increasing-subsequence/
 
+// this solution can produce lis' index.
 export function lengthOfLIS(nums: number[]): number {
-  const dp = new Array(nums.length).fill(1)
+  const dp = new Array(nums.length).fill(1).map((_, index) => {
+    return [index]
+  })
+  let result = [0]
   for (let i = 1; i < nums.length; i++) {
     for (let j = 0; j < i; j++) {
-      if (nums[i] > nums[j]) {
-        dp[i] = Math.max(dp[i], dp[j] + 1)
+      if (nums[j] < nums[i]) {
+        const newDpi = [...dp[j], i]
+        if (newDpi.length > dp[i].length) {
+          dp[i] = newDpi
+        }
+        if (dp[i].length > result.length) {
+          result = dp[i]
+        }
       }
     }
   }
-  return Math.max(...dp)
+
+  return result.length
 }
+
+// this's a better solution than above
+// export function lengthOfLIS(nums: number[]): number {
+//   const dp = new Array(nums.length).fill(1)
+//   for (let i = 1; i < nums.length; i++) {
+//     for (let j = 0; j < i; j++) {
+//       if (nums[i] > nums[j]) {
+//         dp[i] = Math.max(dp[i], dp[j] + 1)
+//       }
+//     }
+//   }
+//   return Math.max(...dp)
+// }
