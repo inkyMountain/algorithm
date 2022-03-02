cd176de65e6a54e2e4bc5db4f6518cd521661624
chenyitao <chenyitao@zhuanzhuan.com>
2022-03-03 00:09:21 +0800

    feat: update paper comment

--- src/paper.ts
+++ src/paper.ts
@@ -7,6 +7,15 @@ import {ListNode} from './leetcode/public'
  * previous 为 left 前面一个 node，
  * current 为首次操作时的 previous 后面一个 node，后续不变。
  * next 始终为 current 后一个 node。
+ * 
+ * 至于 next 指向改变的顺序，首先列出三个 next 的目标：
+ * - next.next
+ * - previous.next
+ * - current
+ * 由于前两个都需要next来访问，所以需要优先处理。
+ * 假设处理 previous.next，让其作为 next.next 的目标，那么 next 后面的 node 就无法再访问了，
+ * 所以目标 next.next 是最先需要被指的，
+ * 其次是 previous.next。
  */
 function reverseBetween(
   head: ListNode | null,
