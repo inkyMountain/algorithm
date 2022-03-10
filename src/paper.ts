c652098e6576d5728494afda8e7c8a1e6fdbee8b
chenyitao <chenyitao@zhuanzhuan.com>
2022-03-10 09:20:01 +0800

    feat: 剑指offer33

--- src/paper.ts
+++ src/paper.ts
@@ -1,82 +1,19 @@
-/*
- * @lc app=leetcode.cn id=449 lang=typescript
- *
- * [449] 序列化和反序列化二叉搜索树
- */
-class TreeNode {
-  val: number
-  left: TreeNode | null
-  right: TreeNode | null
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = val === undefined ? 0 : val
-    this.left = left === undefined ? null : left
-    this.right = right === undefined ? null : right
-  }
-}
-
-// @lc code=start
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
-
-function serialize(root: TreeNode | null): string {
-  let result = ''
-  function traverse(node: TreeNode) {
-    if (node === null) {
-      return ''
-    }
-    result += `,${node.val}`
-    traverse(node.left)
-    traverse(node.right)
-  }
-
-  traverse(root)
-
-  return result.substring(1)
-}
-
-function deserialize(data: string): TreeNode | null {
-  if (!data) {
-    return null
-  }
-  const nums = data.split(',')
+function verifyPostorder(postorder: number[]): boolean {
+  postorder.reverse()
   let index = 0
-  function traverse(lower: number, higher: number) {
-    if (index >= nums.length) {
-      return null
+  function traverse(low: number, high: number) {
+    if (low > high || index >= postorder.length) {
+      return
     }
-    const num = parseInt(nums[index])
-    if (num <= lower || num >= higher) {
-      return null
+    const num = postorder[index]
+    if (num < low || num > high) {
+      return
     }
-    const node = new TreeNode(num)
+
     index++
-    node.left = traverse(lower, num)
-    node.right = traverse(num, higher)
-    return node
+    traverse(num + 1, high)
+    traverse(low, num - 1)
   }
-  return traverse(-Infinity, Infinity)
+  traverse(-Infinity, Infinity)
+  return index === postorder.length
 }
-
-/**
- * Your functions will be called as such:
- * deserialize(serialize(root));
- */
-// @lc code=end
-
-const result = deserialize(serialize(null))
-// const result = deserialize(
-//   serialize(new TreeNode(2, new TreeNode(1), new TreeNode(3)))
-// )
-
-console.log(result)
