00a69d7087ee441355808ff80ab9c7ce0bac8e2c
chenyitao <chenyitao@zhuanzhuan.com>
2022-03-14 18:56:17 +0800

    leetcode: 223 题干

--- src/paper.ts
+++ src/paper.ts
@@ -1,36 +1,18 @@
 /*
- * @lc app=leetcode.cn id=115 lang=typescript
+ * @lc app=leetcode.cn id=223 lang=typescript
  *
- * [115] 不同的子序列
+ * [223] 矩形面积
  */
 
 // @lc code=start
-function numDistinct(s: string, t: string): number {
-  const dp = []
-  for (let i = -1; i < s.length; i++) {
-    dp[i] = []
-    for (let j = -1; j < t.length; j++) {
-      if (j === -1) {
-        dp[i][j] = 1
-      } else {
-        dp[i][j] = 0
-      }
-    }
-  }
-
-  for (let i = 0; i < s.length; i++) {
-    for (let j = 0; j < t.length; j++) {
-      if (s[i] === t[j]) {
-        dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
-      } else {
-        dp[i][j] = dp[i - 1][j]
-      }
-    }
-  }
-
-  return dp[s.length - 1][t.length - 1]
-}
+function computeArea(
+  ax1: number,
+  ay1: number,
+  ax2: number,
+  ay2: number,
+  bx1: number,
+  by1: number,
+  bx2: number,
+  by2: number
+): number {}
 // @lc code=end
-
-// should be 2
-console.log(numDistinct('aabcba', 'abc'))
