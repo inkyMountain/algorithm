31c32108cc345666776b0c3cee73aed319364707
chenyitao <chenyitao@zhuanzhuan.com>
2022-03-15 11:08:11 +0800

    leetcode: max-dot-product-of-two-subsequences

--- src/leetcode/max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.ts
+++ src/leetcode/max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.ts
@@ -1,11 +1,5 @@
-/*
- * @lc app=leetcode.cn id=1458 lang=typescript
- *
- * [1458] 两个子序列的最大点积
- */
-
-// @lc code=start
-function maxDotProduct(nums1: number[], nums2: number[]): number {
+// leetcode 1458
+export function maxDotProduct(nums1: number[], nums2: number[]): number {
   const length1 = nums1.length
   const length2 = nums2.length
   const dp = new Array(length1).fill(-Infinity).map(() => {
@@ -13,6 +7,7 @@ function maxDotProduct(nums1: number[], nums2: number[]): number {
   })
   dp[0][0] = nums1[0] * nums2[0]
   let previous = dp[0][0]
+  // dp的初始化，取nums1的第一个数字，分别和nums2的j长度取最大值。
   for (let j = 1; j < length2; j++) {
     dp[0][j] = Math.max(previous, nums1[0] * nums2[j])
     previous = dp[0][j]
@@ -27,14 +22,18 @@ function maxDotProduct(nums1: number[], nums2: number[]): number {
       const digit1 = nums1[i]
       const digit2 = nums2[j]
       const product = digit1 * digit2
+      // 可能的来源是，，或仅一个加入点积
       dp[i][j] = Math.max(
+        // ij都加入点积，但不与前面取和。
         product,
+        // ij都加入点积，与前面取和。
         dp[i - 1][j - 1] + product,
+        // ij仅一个加入点积。
         dp[i - 1][j],
+        // ij仅一个加入点积。
         dp[i][j - 1]
       )
     }
   }
   return dp[length1 - 1][length2 - 1]
 }
-// @lc code=end
