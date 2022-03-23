5933cb0f1c3f3d29b42f8cfbca9cf80137a40e09
chenyitao <chenyitao@zhuanzhuan.com>
2022-03-23 21:24:11 +0800

    leetcode: inorder successor in bst 2

--- src/leetcode/inorder-successor-in-bst-ii.ts
+++ src/leetcode/inorder-successor-in-bst-ii.ts
@@ -22,34 +22,30 @@ class Node {
   }
 }
 
+/**
+ * 考虑三种情况：
+ * - node 变量是否有右子节点
+ * - 如果没有右子节点，它是父节点的左节点还是右节点。
+ * - 如果 node 是 root。
+ */
 export function inorderSuccessor(node: Node | null): Node | null {
-  function mostLeftChildOf(node: Node | null) {
-    while (node && node.left) {
+  if (node.right) {
+    node = node.right
+    while (node.left) {
       node = node.left
     }
     return node
   }
-  if (node.parent === null) {
-    return mostLeftChildOf(node.right)
-  }
-  if (node.parent.left === node) {
-    return node.right ? mostLeftChildOf(node.right) : node.parent
-  }
 
-  if (node.parent.right === node) {
-    if (node.right) {
-      return mostLeftChildOf(node.right)
-    } else {
-      let current = node
-      while (true) {
-        if (current.parent === null) {
-          return null
-        } else if (current.parent.left === current) {
-          return current.parent
-        } else {
-          current = current.parent
-        }
-      }
-    }
+  /**
+   * 找到第一个为左节点的节点，返回其父节点。
+   * 这概括了几种情况：
+   * - node 是没有右子节点的 root
+   * - node 是没有右子节点，且是某个节点的右节点
+   * - node 是没有右子节点，且是某个节点的左节点
+   */
+  while (node.parent && node.parent.left !== node) {
+    node = node.parent
   }
+  return node.parent
 }
