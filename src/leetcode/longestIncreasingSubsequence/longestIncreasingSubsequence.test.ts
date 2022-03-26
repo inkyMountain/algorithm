cf867e2dc4964f32b46a7252dad8974cd94b8a7e
chenyitao <chenyitao@zhuanzhuan.com>
2022-03-26 15:09:13 +0800

    review: longest increasing subsequence

--- src/leetcode/longestIncreasingSubsequence/longestIncreasingSubsequence.test.ts
+++ src/leetcode/longestIncreasingSubsequence/longestIncreasingSubsequence.test.ts
@@ -14,18 +14,20 @@ describe('longestIncreasingSubsequence', () => {
   })
 })
 
-// describe('longestIncreasingSubsequence', () => {
-//   it('longestIncreasingSubsequence0', () => {
-//     expect(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])).toStrictEqual([
-//       2, 3, 5, 6,
-//     ])
-//   })
+// 测试最长上升子序列具体的值
+describe('longestIncreasingSubsequenceContent', () => {
+  it('longestIncreasingSubsequence0', () => {
+    expect(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])).toStrictEqual([
+      // 2, 3, 5, 6,
+      2, 3, 7, 18
+    ])
+  })
 
-//   it('longestIncreasingSubsequence1', () => {
-//     expect(lengthOfLIS([0, 1, 0, 3, 2, 3])).toStrictEqual([0, 1, 4, 5])
-//   })
+  it('longestIncreasingSubsequence1', () => {
+    expect(lengthOfLIS([0, 1, 0, 3, 2, 3])).toStrictEqual([0, 1, 2, 3])
+  })
 
-//   it('longestIncreasingSubsequence2', () => {
-//     expect(lengthOfLIS([2, 2, 2, 2, 2])).toStrictEqual([0])
-//   })
-// })
+  it('longestIncreasingSubsequence2', () => {
+    expect(lengthOfLIS([2, 2, 2, 2, 2])).toStrictEqual([2])
+  })
+})
--- src/leetcode/longestIncreasingSubsequence/longestIncreasingSubsequence.ts
+++ src/leetcode/longestIncreasingSubsequence/longestIncreasingSubsequence.ts
@@ -1,27 +1,60 @@
 // https://leetcode-cn.com/problems/longest-increasing-subsequence/
 
-// this solution can produce lis' index.
 export function lengthOfLIS(nums: number[]): number {
-  const dp = new Array(nums.length).fill(1).map((_, index) => {
-    return [index]
-  })
-  let result = [0]
-  for (let i = 1; i < nums.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (nums[j] < nums[i]) {
-        const newDpi = [...dp[j], i]
-        if (newDpi.length > dp[i].length) {
-          dp[i] = newDpi
+  const d = []
+  const subsequence = []
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i]
+    if (d.length === 0) {
+      d.push(num)
+      continue
+    }
+    if (num > d[d.length - 1]) {
+      d.push(num)
+      subsequence.push(num)
+    } else if (num < d[d.length - 1]) {
+      let left = 0,
+        right = d.length - 1,
+        index = d.length
+      while (left <= right) {
+        const mid = (left + right) >> 1
+        if (d[mid] >= num) {
+          index = mid
+          right = mid - 1
+        } else {
+          left = mid + 1
         }
-        if (dp[i].length > result.length) {
-          result = dp[i]
-        }
+      }
+      if (index >= 0 && index < d.length) {
+        d[index] = num
       }
     }
   }
-
-  return result.length
+  return d.length
 }
+
+// this solution can produce lis' index.
+// export function lengthOfLIS(nums: number[]): number {
+//   const dp = new Array(nums.length).fill(1).map((_, index) => {
+//     return [index]
+//   })
+//   let result = [0]
+//   for (let i = 1; i < nums.length; i++) {
+//     for (let j = 0; j < i; j++) {
+//       if (nums[j] < nums[i]) {
+//         const newDpi = [...dp[j], i]
+//         if (newDpi.length > dp[i].length) {
+//           dp[i] = newDpi
+//         }
+//         if (dp[i].length > result.length) {
+//           result = dp[i]
+//         }
+//       }
+//     }
+//   }
+
+//   return result.length
+// }
 
 // this's a better solution than above
 // export function lengthOfLIS(nums: number[]): number {
