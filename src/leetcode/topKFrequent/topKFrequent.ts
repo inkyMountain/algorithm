69e9f226dbeaf584d488bc934c8521433445952c
chenyitao <chenyitao@zhuanzhuan.com>
2022-04-05 15:58:07 +0800

    redo: top k frequent

--- src/leetcode/topKFrequent/topKFrequent.ts
+++ src/leetcode/topKFrequent/topKFrequent.ts
@@ -1,5 +1,11 @@
 // https://leetcode-cn.com/problems/top-k-frequent-elements/
 
+/**
+ * 1. 使用排序，时间复杂度 nlogn
+ * 2. 使用优化的快速排序，最优情况下时间复杂度为 O(n)，最坏情况下时间复杂度为 O(n^2)。
+ * 3. 使用最小堆，保存前 k 大的数字，时间复杂度为 O(nlogk)。
+ */
+
 export function topKFrequent(nums: number[], k: number): number[] {
   const frequencyCount = {};
   for (let i = 0; i < nums.length; i++) {
