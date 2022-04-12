c23e7f18fc06cd82b24335f460c46165db018f72
chenyitao <chenyitao@zhuanzhuan.com>
2022-04-12 18:53:08 +0800

    WIP leetcode: decode string

---
+++ src/leetcode/decode-string.ts
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=394 lang=typescript
+ *
+ * [394] 字符串解码
+ */
+
+// @lc code=start
+/**
+ * 1[a2[b3[c]4[d]]]
+ * 1[abcccddddbcccdddd]
+ * digits:
+ * letters:  abcccddddbcccdddd
+ * brackets:
+ *
+ * 1[a2[b]]
+ * digits:   1
+ * letters:  abb
+ * brackets: [
+ * 
+ * 解决方法是通过遍历，使用三个栈来保存遍历过程中遇到的元素。
+ * 它们分别是 digits(存储数字) letters(存储字母) brackets(存储括号)
+ * 当遇到数字时就推入 digits 数组，遇到字母就推入 letters 数组。
+ * 当遇到左括号，推入 brackets 数组。
+ * 但如果是右括号，则从 digits 和 letters 各弹出一个元素(记数字为 num，字母为 letter)，
+ * 将 letter 重复 num 遍，再与 letters 栈顶元素相加，然后继续遍历，直到遍历完成。
+ * 此时返回 letters[0] 就是答案。
+ */
+function decodeString(s: string): string {
+  const digits: number[] = [],
+    letters: string[] = [],
+    brackets: string[] = []
+  
+}
+// @lc code=end
